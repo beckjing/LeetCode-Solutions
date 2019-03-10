312ebc436c1858dec35824721998707734f0af68
beckjing <176547352@qq.com>
2019-03-10 10:04:29 +0800

    update

---
+++ 113/PathSumTwo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-01-16 22:48
+# @Author  : Yuecheng Jing
+# @Site    : www.nanosparrow.com
+# @File    : PathSumTwo
+# @Software: PyCharm
+
+class Solution:
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+
+if __name__ == '__main__':
