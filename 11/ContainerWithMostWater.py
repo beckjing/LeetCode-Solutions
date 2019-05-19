1f6257167692e7718e391238276a96e48eaa6300
beckjing <176547352@qq.com>
2019-05-19 23:30:56 +0800

    finish11

--- 11/ContainerWithMostWater.py
+++ 11/ContainerWithMostWater.py
@@ -12,9 +12,28 @@ class Solution:
         :type height: List[int]
         :rtype: int
         """
+        result = 0
+        leftIndex = 0
+        rightIndex = len(height) - 1
+        leftHeight = height[leftIndex]
+        rightHeight = height[rightIndex]
+        while leftIndex < rightIndex:
+            if leftHeight > rightHeight:
+                if result < (rightHeight * (rightIndex - leftIndex)):
+                    result = (rightHeight * (rightIndex - leftIndex))
+                rightIndex -= 1
+                rightHeight = height[rightIndex]
+            else:
+                if result < (leftHeight * (rightIndex - leftIndex)):
+                    result = (leftHeight * (rightIndex - leftIndex))
+                leftIndex += 1
+                leftHeight = height[leftIndex]
+        return result
+            
+            
         
 
 if __name__ == '__main__':
     test = Solution()
-
+    print(test.maxArea([1,8,6,2,5,4,8,3,7]))
 
